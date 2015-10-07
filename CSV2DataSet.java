d40aa1a2292770e592259d43c487e1e2c859c9cf
vyas <vyas@Vyassu>
2015-10-07 17:17:32 -0400

    Commenting of the code along with minor changes in the loadfile function

--- CSV2DataSet.java
+++ CSV2DataSet.java
@@ -13,41 +13,44 @@ import org.nd4j.linalg.factory.BaseNDArrayFactory;
 import org.nd4j.linalg.factory.Nd4j;
 
 public class CSV2DataSet {
-	INDArray test = Nd4j.create(345677, 1);
-	INDArray test1 = Nd4j.create(345677, 1);
+	INDArray label = Nd4j.create(345677, 1);
+	INDArray feature = Nd4j.create(345677, 1);
 	DataSet inputDataSet  = null;
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			CSV2DataSet test = new CSV2DataSet();
-			test.loadFile();
-	}
-	
-	public void loadFile() {
-
+		// Creating class variable 
 		String csvFile = "C://Users/vyas/Downloads/2013/2013-04-10.csv";
+			CSV2DataSet test = new CSV2DataSet();
+			test.loadFile(csvFile);
+	}
+	/*
+	 * Function to convert CSV data into DATASET class variable
+	 * 
+	 */
+	public DataSet loadFile(String Filename) {	
 		BufferedReader br = null;
 		String line = "";
 		String cvsSplitBy = ",";
 		int count = 0;
-		try {
-			br = new BufferedReader(new FileReader(csvFile));
-			while ((line = br.readLine()) != null) {
-
-			        // use comma as separator
-				if (count >0)
-				{String[] data = line.split(cvsSplitBy);
-				
-                test.putScalar(count,Integer.parseInt(data[4]));
-                test1.putScalar(count,Integer.parseInt(data[6]));
-				System.out.println("Failure " + data[4]
-	                                 + " , Input=" + data[6] + "]");
+		try 
+		{
+			br = new BufferedReader(new FileReader(Filename));   // Read each line from the input CSV
+			// Loop till end of File
+			while ((line = br.readLine()) != null) 
+			{
+			   if (count >0)  // Condition to filter out row1 from the Excel sheet
+				{
+				   String[] data = line.split(cvsSplitBy);
+				   label.putScalar(count,Integer.parseInt(data[4]));
+				   feature.putScalar(count,Integer.parseInt(data[6]));
+				   //System.out.println("Failure " + data[4]
+	                                // + " , Input=" + data[6] + "]");
 				}
 				count++;
-
 			}
-			inputDataSet = new DataSet(test, test1);
-			System.out.println(inputDataSet);
+			inputDataSet = new DataSet(label, feature);
+			//System.out.println(inputDataSet);
+			
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -62,5 +65,6 @@ public class CSV2DataSet {
 				}
 			}
 		}
+		return inputDataSet;
 	}
 }
